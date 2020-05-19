42e4a078b771921446c2de6b0e85d4a4632c2205
slsius <ssiusinglung@gmail.com>
2020-05-19 14:17:36 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -1,9 +1,9 @@
-from datetime import date
+from datetime import datetime
 from futu import *
 
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111) #make connection
 
-today = today.strftime("%Y-%m-%d")  #declare today with suitable format
+today = datetime.today().strftime("%Y-%m-%d")  #declare today with suitable format
 
 print('----------------------------') #split line
 
