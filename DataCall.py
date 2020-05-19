2ab1d391178937f7707ef3e80a90d85f41fdd7e9
slsius <ssiusinglung@gmail.com>
2020-05-19 17:45:33 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -7,19 +7,45 @@ quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111) #make connection
 
 today = datetime.today().strftime("%Y-%m-%d")  #declare today with suitable format
 
+print(today-1)
+
 print('----------------------------') #split line
 
 print(quote_ctx.get_market_snapshot('HK.00700')) #get snap shot
 
 print('----------------------------') #split line
 
-ret, data, page_req_key = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=10, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+#data set 1
+ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data.time_key, data.open) #end='' is today
+
 print('----------------------------') #split line
 
-df = pd.DataFrame(data)
-df.to_csv('data.csv', encoding='utf-8', index=False)
+#data set 2
+ret2, data2, page_req_key2 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+print(data.time_key, data.open)
+
+#data set 3
+ret3, data3, page_req_key3 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+print(data.time_key, data.open)
+
+#data set 4
+ret4, data4, page_req_key4 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+print(data.time_key, data.open)
+
+#data set 5
+ret5, data5, page_req_key5 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+print(data.time_key, data.open)
+
+#data set 6
+ret6, data6, page_req_key6 = quote_ctx.request_history_kline('HK.00700', start=today, end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+print(data.time_key, data.open)
+
+'''
+df = pd.DataFrame(data) #insert data to panda frame
+df.to_csv('data.csv', encoding='utf-8', index=False) #write all the data to csv
 
 print('----------------------------')
+'''
 
 quote_ctx.close()
