c97895dcb001f6877ea100303bfbd6afbc1ce22d
slsius <ssiusinglung@gmail.com>
2020-05-19 18:00:25 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -21,34 +21,34 @@ print('----------------------------') #split line
 
 print('-------DAY 1--------')
 #data set 1
-ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data1.time_key, data1.open) #end='' is today
 
 print('----------------------------') #split line
 
 #data set 2
 print('-------DAY 2--------')
-ret2, data2, page_req_key2 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=1)), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret2, data2, page_req_key2 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=1)), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data2.time_key, data2.open)
 
 #data set 3
 print('-------DAY 3--------')
-ret3, data3, page_req_key3 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=2)), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret3, data3, page_req_key3 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=2)), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data3.time_key, data3.open)
 
 #data set 4
 print('-------DAY 4--------')
-ret4, data4, page_req_key4 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=3)), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret4, data4, page_req_key4 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=3)), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data4.time_key, data4.open)
 
 #data set 5
 print('-------DAY 5--------')
-ret5, data5, page_req_key5 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=4)), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret5, data5, page_req_key5 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=4)), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data5.time_key, data5.open)
 
 #data set 6
 print('-------DAY 6--------')
-ret6, data6, page_req_key6 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=5)), end='', max_count=120, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret6, data6, page_req_key6 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=5)), end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 print(data6.time_key, data6.open)
 
 '''
