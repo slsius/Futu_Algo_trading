6e96fa0eb6d91b53cdcfbe10a71e03dc315945ef
slsius <ssiusinglung@gmail.com>
2020-05-19 15:35:43 +0800

    Update MLsample.py

--- MLsample.py
+++ MLsample.py
@@ -1,42 +1,24 @@
-# Load libraries
-
+# compare algorithms
 from pandas import read_csv
-from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-
-# box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-pyplot.show()
-# histograms
-#dataset.hist()
-#pyplot.show()
-# scatter plot matrix
-#scatter_matrix(dataset)
-#pyplot.show()
-
 # Split-out validation dataset
 array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
-
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
 # Spot Check Algorithms
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
@@ -49,14 +31,11 @@ models.append(('SVM', SVC(gamma='auto')))
 results = []
 names = []
 for name, model in models:
-	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	kfold = StratifiedKFold(n_splits=10, random_state=1)
 	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
 	results.append(cv_results)
 	names.append(name)
-	print('---------------------------------------------')
-	print('%s : %f %f' % (name, cv_results.mean(), cv_results.std()))
-	print('---------------------------------------------')
-	
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 # Compare Algorithms
 pyplot.boxplot(results, labels=names)
 pyplot.title('Algorithm Comparison')
