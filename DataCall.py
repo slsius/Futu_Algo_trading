65d046c8ccfa82ee939e7203da01d7abf6b010bc
slsius <ssiusinglung@gmail.com>
2020-05-20 14:59:14 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -74,7 +74,7 @@ print(RSI)
 Nem =    (data1.close - data1.open) + 
        2*(data1.close.shift(1) - data1.open.shift(1)) + 
        2*(data1.close.shift(2) - data1.open.shift(2)) + 
-         (data1.close.shift(3) - data1.open.shift(3)))
+         (data1.close.shift(3) - data1.open.shift(3))
 Dem =    data1.high - data1.low +
       2*(data1.high.shift(1) - data1.low.shift(1)) +
       2*(data1.high.shift(2) - data1.low.shift(2)) +
