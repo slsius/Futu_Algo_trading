477a56e575626d8f3f9ee6077b94d935b3dc112c
slsius <ssiusinglung@gmail.com>
2020-05-20 16:11:18 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -42,7 +42,7 @@ print(temp)
 
 #RSI test
 RSI = abstract.RSI(data1.close,6)
-RSIData = pd.DataFrame(RSI)
+RSIData = pd.DataFrame(RSI, columns='data')
 
 #RVI test
 
@@ -52,3 +52,4 @@ Dem =data1.high-data1.low+2*(data1.high.shift(1) - data1.low.shift(1)) +2*(data1
 RVI = (Nem/6)/(Dem/6)
 RVIR = (RVI + 2*RVI.shift(1) + 2*RVI.shift(2) + RVI.shift(3))/6
 print('------------------rvi---------------------')
+print(RSIData)
