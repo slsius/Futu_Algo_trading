9f1ed7723340b5a00a087e94f977d1cd0b765275
slsius <ssiusinglung@gmail.com>
2020-05-22 16:50:31 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -137,11 +137,12 @@ mpf.plot(pdata)
 '''
 
 
-plotdata1['Ppen'] = data1.open
+plotdata1['Open'] = data1.open
 plotdata1['High'] = data1.high
 plotdata1['Low'] = data1.low
 plotdata1['Close'] = data1.close
 plotdata1['Volume'] = data1.volume
+plotdata1.set_index('Date', inplace=True)
 print(plotdata1)
 print(plotdata1.dtypes)
 mpf.plot(plotdata1)
