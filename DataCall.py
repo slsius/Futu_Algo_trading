f3c18b9cf246d149806bfb77ebfda0c737cc4aed
slsius <ssiusinglung@gmail.com>
2020-05-26 07:59:38 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -212,6 +212,12 @@ class RVICross(bt.Strategy):
         #sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
         #sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving averag
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6
+        print('----------------show data---------------')
+        print(self.data.close[-1])
+        print(self.data.open[-1])
+        print(self.data.close[-2])
+        print(self.data.open[-2])
+        
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         RVI = (NUM/6)/(DEM/6)
         try:
