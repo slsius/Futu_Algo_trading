097d6f88cc2d6ad4572b38d52b8b433213392c61
slsius <ssiusinglung@gmail.com>
2020-05-25 15:15:35 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -214,7 +214,14 @@ class RVICross(bt.Strategy):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         RVI = (NUM/6)/(DEM/6)
-        RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+        while True:
+          try:
+            RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+            break
+          except IndexError
+            RVIR = 0
+            
+            
         RSI6 = self.sma = bt.talib.RSI(self.data, timeperiod=self.p.RSIPer)
         self.crossover = bt.ind.CrossOver(RVI, RVIR)  # crossover signal
 
