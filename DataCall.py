922b80c59d79e765daf93d30a2e71b2eee47e296
slsius <ssiusinglung@gmail.com>
2020-05-25 15:22:49 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -214,13 +214,12 @@ class RVICross(bt.Strategy):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         RVI = (NUM/6)/(DEM/6)
-        while True:
-          try:
-            RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
-            break
-          except IndexError:
-            print('error catch')
-            RVIR = 0
+        try:
+          RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+          break
+        except IndexError:
+          print('error catch')
+          RVIR = 0
             
             
         RSI6 = self.sma = bt.talib.RSI(self.data, timeperiod=self.p.RSIPer)
