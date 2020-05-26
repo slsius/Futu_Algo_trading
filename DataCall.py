090611d9c48b4010a326f89c40a9b8f5c4bd4110
slsius <ssiusinglung@gmail.com>
2020-05-26 14:30:55 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -213,7 +213,7 @@ class RVIin(bt.Indicator):
           self.lines.RVIR = RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
         except IndexError:
           print('error catch')
-          self.lines.RVIR = RVIR = 0
+          self.lines.RVIR = RVIR = None
         
         
         
