82840da36ebe4cab819479767ca725767068c169
slsius <ssiusinglung@gmail.com>
2020-05-26 15:09:05 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -198,6 +198,7 @@ class PandasData(bt.feed.DataBase):
         ('close', 'Close'),
         ('volume', 'Volume'),
     )
+    
 class RVIin(bt.Indicator):
     lines = ('RVI','RVIR')
     plotinfo = dict(subplot=True)
@@ -218,7 +219,10 @@ class RVIin(bt.Indicator):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6  
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         self.lines.RVI[0] = RVIval = (NUM/6)/(DEM/6)
-        self.lines.RVIR[0] = RVIRval = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+        try
+          self.lines.RVIR[0] = RVIRval = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+        except (IndexError, KeyError)
+          self.lines.RVIR[0] = 0
         dif = RVIval - RVIRval
         
         
