babc41e2507bb5c2d0dbb4e89ffe34f431cb5b1a
slsius <ssiusinglung@gmail.com>
2020-05-26 13:18:54 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -208,7 +208,7 @@ class RVI(bt.Indicator):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6  
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         self.lines.RVI = RVI = (NUM/6)/(DEM/6)
-        self.lines.RVIR = =RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+        self.lines.RVIR = RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
         #try:
         #self.RVIR = RVIR = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
         '''
