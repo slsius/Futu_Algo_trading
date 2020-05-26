30445e2b9751a75130dc27a1fe0eaddea7bc8323
slsius <ssiusinglung@gmail.com>
2020-05-26 15:30:40 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -220,15 +220,15 @@ class RVIin(bt.Indicator):
     def next(self):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6  
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
-        self.lines.RVI[0] = RVIval = (NUM/6)/(DEM/6)
+        self.lines.RVI[0] = self.RVIval = (NUM/6)/(DEM/6)
         try:
-          self.lines.RVIR[0] = RVIRval = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
+          self.lines.RVIR[0] = self.RVIRval = (RVI + 2*RVI[-1] + 2*RVI[-2] + RVI[-3])/6
         except (IndexError, KeyError):
           self.lines.RVIR[0] = RVIRval= 0
     def getdiv(self):
-        diver = RVIval - RVIRval
-        print('RVI value' + RVIval)
-        print(RVIRval)
+        diver = self.RVIval - self.RVIRval
+        print('RVI value' + self.RVIval)
+        print(self.RVIRval)
         return diver
         
         
