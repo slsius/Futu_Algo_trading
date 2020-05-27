3c96ddace9eab7e7f21ba0057d5e141ad0d18c74
slsius <ssiusinglung@gmail.com>
2020-05-27 13:13:54 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -270,7 +270,7 @@ class RVIin(bt.Indicator):
           if(self.crossover > 0):  
             self.lines.sigin[0] = 10
         elif (self.flag):
-          if(self.crossover < 0)
+          if(self.crossover < 0):
           self.lines.sigout[0] = -10
         '''
         self.lines.RVI[0] = self.data.RVI
