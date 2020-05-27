41a5bfcff8ee419ffb505c9045713bd092947218
slsius <ssiusinglung@gmail.com>
2020-05-27 11:04:11 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -298,7 +298,7 @@ class RVICross(bt.Strategy):
         
         print('---rsi')
         print(self.tarsi0)
-        self.line.tarsi0[-1]
+        self.tarsi0.line[-1]
         '''
         if ((self.tarsi0 > 0) and (self.tarsi1 > 0) and (self.tarsi2 > 0) and (self.tarsi3 > 0)):
           self.tarsi3 = self.tarsi2
