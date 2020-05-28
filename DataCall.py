91f603cc23369b2c663f2dc4ea0798e6e2ceb78d
slsius <ssiusinglung@gmail.com>
2020-05-28 14:25:44 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -223,6 +223,12 @@ class RVICross(bt.Strategy):
         print('check3')
         
     def next(self): 
+      if not self.position:  # not in the market
+        if self.crossover > 0:  # if fast crosses slow to the upside
+          self.buy()  # enter long
+        elif self.crossover < 0:  # in the market & cross to the downside
+          self.close()  # close long position
+            '''
         print('check$$')
         print(self.crossover)
         if not self.position:  # not in the market
@@ -233,7 +239,7 @@ class RVICross(bt.Strategy):
             elif self.crossover < 0: #and (self.btsma or self.btsma1 or self.btsma2 or self.btsma3)>= self.p.RSIHi:
         #elif self.crossover < 0 and self.cus.RSI >= self.p.RSIHi:  # in the market & cross to the downside
               self.close()  # close long position
-             
+             '''
 
 def runstrat():
     args = parse_args()
