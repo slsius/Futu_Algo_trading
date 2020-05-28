1a41071b08648a13b1977d0d5bb82a34b2399278
slsius <ssiusinglung@gmail.com>
2020-05-28 15:59:08 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,36 +1,6 @@
 import backtrader as bt
 import backtrader.indicators as btind
 
-'''
-class RSIcus(bt.Indicator):
-    lines = ('RSIcus','rsiup','rsidown')
-    plotinfo = dict(subplot=True)
-    params = (('period', 7),('rsip',6),)
-    
-    def __init__(self):
-        self.addminperiod(self.params.period)
-        self.movup = 0
-        self.movdown = 0
-    def next(self):
-        for x in range(0, -6, -1):
-            if (self.data.close[x] - self.data.close[x-1]) > 0:
-              self.movup = self.movup + self.data.close[x] - self.data.close[x-1]
-            else:
-              self.movdown = self.movdown + self.data.close[x-1] - self.data.close[x]
-        rs = (self.movup/self.p.rsip)/(self.movdown/self.p.rsip)
-        self.lines.RSIcus[0] = 100 - 100 / ( 1 + rs)
-        if(self.lines.RSIcus >=60 or self.lines.RSIcus[-1] >=60 or self.lines.RSIcus[-2] >=60):
-          self.lines.rsiup[0] = 50
-        else:
-          self.lines.rsiup[0] = 0
-        
-        if(self.lines.RSIcus <=20 or self.lines.RSIcus[-1] <=20 or self.lines.RSIcus[-2] <=20):
-          self.lines.rsidown[0] = 50
-        else:
-          self.lines.rsidown[0] = 0
- 
-'''
-
 class RVIin(bt.Indicator):
     lines = ('RVI','RVIR','sigin','sigout')
     plotinfo = dict(subplot=True)
@@ -38,9 +8,8 @@ class RVIin(bt.Indicator):
 
     def __init__(self):
         self.addminperiod(self.params.period)
-
-        self.tarsi0 = bt.indicators.RSI(self.data, period= self.p.RSIPer)
-        self.crossover = bt.ind.CrossOver(self.RVI,self.RVIR)
+        
+        
     def next(self):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6  
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
@@ -50,10 +19,28 @@ class RVIin(bt.Indicator):
         except (IndexError, KeyError):
           self.lines.RVIR[0] = 0
         
+       
+                
+class Buyin(bt.Indicator):
+    lines = (sigin','sigout')
+    plotinfo = dict(subplot=True)
+    params = (('period', 4),('Hi',60),('Lo',20),('RSIPer',6))
+             
+    def __init__(self):
+        self.addminperiod(self.params.period)
+        self.tarsi0 = bt.indicators.RSI(self.data, period= self.p.RSIPer)
+        self.rvidata = RVIin(self.data)
+        self.crossover = bt.ind.CrossOver(self.rvidata.RVI,self.rvidata.RVIR)
+             
+    def next(self):
+             
         if (self.tarsi0 <= self.p.Lo) or (self.tarsi0[-1] <= self.p.Lo) or (self.tarsi0[-2] <= self.p.Lo) or (self.tarsi0[-3] <= self.p.Lo):
             if self.crossover > 0:
                 self.lines.sigin[0] = 0.5
+                print('sigin')
                 
         if (self.tarsi0 >= self.p.Hi) or (self.tarsi0[-1] >= self.p.Hi) or (self.tarsi0[-2] >= self.p.Hi) or (self.tarsi0[-3] >= self.p.Hi):
             if self.crossover > 0:
                 self.lines.sigout[0] = -0.5
+                print('sigout')
+        
