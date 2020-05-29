ee5e1182e20127bee4491bd45159e32fa611697d
slsius <ssiusinglung@gmail.com>
2020-05-29 15:07:34 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -224,9 +224,11 @@ class RVICross(bt.Strategy):
           if self.crossover > 0:
             if (self.tarsi0 <= self.p.RSILo) or (self.tarsi0[-1] <= self.p.RSILo) or (self.tarsi0[-2] <= self.p.RSILo) or (self.tarsi0[-3] <= self.p.RSILo):
               self.buy()
+              print('buy')
           elif self.crossover < 0:
             if (self.tarsi0 >= self.p.RSIHi) or (self.tarsi0[-1] >= self.p.RSIHi) or (self.tarsi0[-2] >= self.p.RSIHi) or (self.tarsi0[-3] >= self.p.RSIHi):
               self.close()
+              print('close')
 
       
         
