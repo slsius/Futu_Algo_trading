df60a087b309a1d7a20e241aa455de314a3e9c86
slsius <ssiusinglung@gmail.com>
2020-05-29 08:56:38 +0800

    Update DataCall.py

--- DataCall.py
+++ DataCall.py
@@ -28,7 +28,7 @@ today = datetime.today()
 NumDay = 50  #variable
 
 #data set 1
-ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=110*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)-400), end=DayStr(today - timedelta(days=NumDay)-365), max_count=110*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 
 if ret1 == RET_OK:
     print(data1)
