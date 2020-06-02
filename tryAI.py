288c344296bb1590c001057c9421c80fb9ea1f25
slsius <ssiusinglung@gmail.com>
2020-06-02 15:10:15 +0800

    Update tryAI.py

--- tryAI.py
+++ tryAI.py
@@ -24,7 +24,7 @@ def DayStr(Tday): #function to return date in specific format
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111) #make connection
 
 today = datetime.today()
-NumDay = 365 #set the number of day of data
+NumDay = 10 #set the number of day of data
 
 #data set 1
 ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=110*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_5M) 
