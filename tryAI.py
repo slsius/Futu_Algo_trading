d2a48b2bd6bead786514f5121e0e9c97fd1a2d7c
slsius <ssiusinglung@gmail.com>
2020-06-03 09:28:18 +0800

    Update tryAI.py

--- tryAI.py
+++ tryAI.py
@@ -189,7 +189,6 @@ class RVICross(bt.Strategy):
             if (self.tarsi0 <= self.p.RSILo) or (self.tarsi0[-1] <= self.p.RSILo) or (self.tarsi0[-2] <= self.p.RSILo) or (self.tarsi0[-3] <= self.p.RSILo):
               self.buy(size = 100)
               print('buy')
-              print(portfolio_value)
               print(self.data.close[0])
               print('^^^')
         elif self.position:
