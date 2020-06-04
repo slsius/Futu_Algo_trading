44fdef4097e4e47011ddbade13af7b1ed314f760
slsius <ssiusinglung@gmail.com>
2020-06-04 15:37:11 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,9 +10,9 @@ import random
 
 #for simulation import
 #from PIL import Image  # for creating visual of our env
-import cv2  # for showing our visual live
+#import cv2  # for showing our visual live
 import matplotlib.pyplot as plplot  # for graphing our mean rewards over time
-import pickle  # to save/load Q-Tables
+#import pickle  # to save/load Q-Tables
 from matplotlib import style  # to make pretty charts because it matters.
 import time 
 
