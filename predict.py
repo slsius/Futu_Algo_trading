8d856a9c1ae76fa0dd7122b0e08d5f2dccaef93c
slsius <ssiusinglung@gmail.com>
2020-06-05 09:58:47 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -149,7 +149,7 @@ print('add data')
 cerebro.adddata(stockdata)
 
 hist = {'RSI period','RSI Hi','RSI Lo','Profit/Loss'}
-df = pd.DataFrame(hist,columns = ['RSI period','RSI Hi','RSI Lo','Profit/Loss'])
+df = pd.DataFrame(,columns = hist)
 for tstperiod in range (20):
   print(tstperiod)
   for tsthi in range(50,100):
