3d2383bca9d1093d724367f6ff43b68ccef76c48
slsius <ssiusinglung@gmail.com>
2020-06-05 16:31:45 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -148,11 +148,11 @@ for tstperiod in range (2,10,2):
       RVICross.RSILo = tstlo
       cerebro.addstrategy(RVICross)
       cerebro.broker.setcash(1000.0)
-      print('Period: %.2F',tstperiod)
+      print('Period: %.2F' % tstperiod)
       print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
       cerebro.run()
       print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-      print('Get Cash %.2f'cerebro.broker.getcash())
+      print('Get Cash %.2f' % cerebro.broker.getcash())
       df = df.append({'RSI period':tstperiod,'RSI Hi':tsthi,'RSI Lo':tstlo,'Profit/Loss':cerebro.broker.getcash()-1000}, ignore_index=True)
 df.to_csv('test_data.csv', encoding='utf-8', index=False) #write all the data to csv      
 # Plot the result
