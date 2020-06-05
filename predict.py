0d8a52cc18b5cb5a7a4f91df8f7c87573cd1d93a
slsius <ssiusinglung@gmail.com>
2020-06-05 09:08:02 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -24,12 +24,12 @@ def DayStr(Tday): #function to return date in specific format
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111) #make connection
 
 today = datetime.today()
-NumDay = 50 #set the number of day of data
+NumDay = 20 #set the number of day of data
 
 
 #data set 1
-#ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=110*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
-ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start='2005-01-01', end='2009-12-31', max_count=5000, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
+ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=110*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_60M) 
+#ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start='2005-01-01', end='2009-12-31', max_count=5000, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
 if ret1 == RET_OK:
     print('ok')
     #print(data1)
@@ -37,152 +37,141 @@ if ret1 == RET_OK:
     #print(data1['close'].values.tolist())   # 第一页收盘价转为list
 else:
     print('error:', data1)
-
-df = pd.DataFrame(data1) #insert data to panda frame
-df.to_csv('alldata.csv', encoding='utf-8', index=False) #write all the data to csv
-'''
-#data2 
-ret1, data2, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start='2019-01-01', end='2019-12-31', max_count=365, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
-if ret1 == RET_OK:
-    print('ok')
-    #print(data1)
-    #print(data1['code'][0])    # 取第一条的股票代码
-    #print(data1['close'].values.tolist())   # 第一页收盘价转为list
-else:
-    print('error:', data2)
-
-df = pd.DataFrame(data2) #insert data to panda frame
-df.to_csv('data2.csv', encoding='utf-8', index=False) #write all the data to csv
-
-quote_ctx.close() #close connection 
-'''
-'''
-data1 = pd.read_csv('data1.csv') 
-data1['time_key']=pd.to_datetime(data1['time_key'])
-#data1.rename(columns={'time_key':'Date'})
-data1.set_index('time_key', inplace=True)
-data1.index.names = ['Date']
-
-data2 = pd.read_csv('data2.csv') 
-data2['time_key']=pd.to_datetime(data1['time_key'])
-data2.set_index('time_key', inplace=True)
-data2.index.names = ['Date']
-'''
-'''
-print(data1.head())
-print('\n Data Types:')
-print(data1.dtypes)
-print(data1)
-'''
-'''
-#Signal
-RSIPeriod = 6
-RSILo = 20
-RSIHi = 60
-signals = pd.DataFrame()
-
-Nem =(data1.close-data1.open)+2*(data1.close.shift(1) - data1.open.shift(1))+2*(data1.close.shift(2) - data1.open.shift(2))+(data1.close.shift(3) - data1.open.shift(3))      
-Dem =data1.high-data1.low+2*(data1.high.shift(1) - data1.low.shift(1)) +2*(data1.high.shift(2) - data1.low.shift(2)) +(data1.high.shift(3) - data1.low.shift(3))
-
-signals['RVI'] = data1['RVI'] = RVI = (Nem/6)/(Dem/6)
-signals['RVIR'] = data1['RVIR'] = (RVI + 2*RVI.shift(1) + 2*RVI.shift(2) + RVI.shift(3))/6
-signals['RVI_diff'] = signals['RVI'] - signals['RVIR']
-
-#RSI -- variable
-signals['RSI'] = abstract.RSI(data1.close,RSIPeriod)
-
-temp1 = signals['RSI'][:-1]
-temp1 = temp1.shift(1)
-temp2 = signals['RSI'][:-2]
-temp2 = temp1.shift(2)
-
-RVIshift1 = signals['RVI_diff'][:-1]
-RVIshift1 = signals['RVI_diff'].shift(1)
-RVIshift2 = signals['RVI_diff'][:-2]
-RVIshift2 = signals['RVI_diff'].shift(2)
-
-RSISignal = np.where((signals['RSI'] <= RSILo) | (temp1 <=RSILo) | (temp2 <=RSILo) , 1.0, 0.0)
-RVISignal = np.where((signals['RVI_diff'] >= 0) & (RVIshift1 <= 0),1.0,0.0)
-
-signals['signal'] = np.where((RSISignal == 1) & (RVISignal == 1),1.0,0.0)
+  
+quote_ctx.close() #close connection
 
 
-SellRSI = np.where((signals['RSI'] >= RSIHi) | (temp1 >=RSIHi) | (temp2 >=RSIHi),1.0,0.0)
-SellRVI = np.where(signals['RVI'] <= signals['RVIR'],1.0,0.0)
-signals['sell'] = np.where((SellRSI == 1) & (SellRVI == 1),1.0,0.0)
-del [[temp1,temp2,RVIshift1,RVIshift2]]
+plotdata1 = pd.DataFrame() #index=pd.to_datetime(data1['time_key'], format='%Y-%m-%d %H:%M:%S', infer_datetime_format=True)
+data1['time_key'] = pd.to_datetime(data1['time_key'],)
 
-#print(signals)
-
-#print(len(data1.index))
-
-#simulation set up
-'''
-'''
-q_table = np.zeros([len(data1.index), 3])
-'''
-'''
+plotdata1['Open'] = data1['open']
+plotdata1['High'] = data1.high
+plotdata1['Low'] = data1.low
+plotdata1['Close'] = data1.close
+plotdata1['Volume'] = data1.volume
+plotdata1.index.name = 'Date'
+plotdata1.rename(columns={'time_key':'Date'})
+plotdata1.index = pd.to_datetime(data1['time_key'], format='%Y-%m-%d %H:%M:%S', infer_datetime_format=True)
 
 
-for i in range(1, 100001):
-  while not done:
-    epochs = 0
-    penalties, reward = 0, 0
-    done = False
-    
-    #do action
+#-----------------------------------Back test-----------------------------------
+class PandasData(bt.feed.DataBase):
+    '''
+    The ``dataname`` parameter inherited from ``feed.DataBase`` is the pandas
+    DataFrame
+    '''
+
+    params = (
+        # Possible values for datetime (must always be present)
+        #  None : datetime is the "index" in the Pandas Dataframe
+        #  -1 : autodetect position or case-wise equal name
+        #  >= 0 : numeric index to the colum in the pandas dataframe
+        #  string : column name (as index) in the pandas dataframe
+        ('datetime', None),
+
+        # Possible values below:
+        #  None : column not present
+        #  -1 : autodetect position or case-wise equal name
+        #  >= 0 : numeric index to the colum in the pandas dataframe
+        #  string : column name (as index) in the pandas dataframe
+        ('open', 'Open'),
+        ('high', 'High'),
+        ('low', 'Low'),
+        ('close', 'Close'),
+        ('volume', 'Volume'),
+    )
 
     
-epochs += 1 
-
-alpha = 0.1
-gamma = 0.6
-epsilon = 0.1
-'''
-'''
-
-HM_EPISODES = 25000
-MOVE_PENALTY = 1
-PROFIT_PENALTY = 20
-LOSS_PENALTY = 100
-epsilon = 0.5  # randomness
-EPS_DECAY = 0.9999  # Every episode will be epsilon*EPS_DECAY
-SHOW_EVERY = 1000 # how often to play through env visually.
-
-start_q_table = None  # if we have a pickled Q table, we'll put the filename of it here.
-
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
-
-class stocker():
-  def __init__(self):
-      return()
-  def __str__(self):
-      return()
-  def __sub__(self, other):
-      return () #data close now- data close buy
-  def action(self, choice): #(3 action)
-      if choice == 0:
-        return()
-      elif choice == 1:
-        #buy
-        buystock(i)
-      elif choiice == 2:
-        #sell
-        sellstock(i)
-  def buystock(i):
-      buyprice = data1.close[i]
-  def sellstock(i): 
-      sellprice = data1.close[i]
-      
-      
-if start_q_table is None:
-   q_table = {}
-else:
-    with open(start_q_table, "rb") as f:
-        q_table = pickle.load(f)
+class RVICross(bt.Strategy, para1,para2):
+    params = dict(
+        RSIHi=para1,  
+        RSILo=para2,   
+        RSIPer=6
+    )
+    
+    
+    def __init__(self):        
+        self.tarsi0 = bt.indicators.RSI(self.data, period= self.p.RSIPer)
+        self.mova = bt.ind.SMA(self.data.close,period = 20)
+        self.IDC = strgy.RVIin(self.data)
+        self.crossover = bt.ind.CrossOver(self.IDC.RVI,self.IDC.RVIR)
         
-episode_rewards = []
-    
-#for episode in range(HM_EPISODES):  
-'''
+    def next(self):
+        if not self.position: 
+          if self.crossover > 0:
+            if (self.tarsi0 <= self.p.RSILo) or (self.tarsi0[-1] <= self.p.RSILo) or (self.tarsi0[-2] <= self.p.RSILo) or (self.tarsi0[-3] <= self.p.RSILo):
+              portfolio_value = self.broker.get_value()
+              self.hand = portfolio_value/self.data.close[0]
+              self.buy(size = self.hand)
+              print('buy')
+              print(portfolio_value)
+              print(self.data.close[0])
+              print('^^^')
+        if self.position:  
+          if self.crossover < 0:
+            if (self.tarsi0 >= self.p.RSIHi) or (self.tarsi0[-1] >= self.p.RSIHi) or (self.tarsi0[-2] >= self.p.RSIHi) or (self.tarsi0[-3] >= self.p.RSIHi):
+              self.close(size = self.hand)
+              print('close')
+              print(self.data.close[0])
+              print('^^^')
+          
+
+      
+        
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Pandas test script')
+
+    parser.add_argument('--noheaders', action='store_true', default=False,
+                        required=False,
+                        help='Do not use header rows')
+
+    parser.add_argument('--noprint', action='store_true', default=False,
+                        help='Print the dataframe')
+
+    return parser.parse_args()
+  
+def runstrat():
+    args = parse_args()
+
+    # Create a cerebro entity
+    cerebro = bt.Cerebro(stdstats=True)
+
+    # Add a strategy
+    cerebro.addstrategy(RVICross)
+    # Get a pandas dataframe
+    #datapath = ('../../datas/2006-day-001.txt')
+
+    # Simulate the header row isn't there if noheaders requested
+    skiprows = 1 if args.noheaders else 0
+    header = None if args.noheaders else 0
+    '''
+    dataframe = pandas.read_csv(datapath,
+                                skiprows=skiprows,
+                                header=header,
+                                parse_dates=True,
+                                index_col=0)
+    '''
+    if not args.noprint:
+        print('--------------------------------------------------!')
+        print(plotdata1)
+        print('--------------------------------------------------!')
+
+    # Pass it to the backtrader datafeed and add it to the cerebro
+    stockdata = bt.feeds.PandasData(dataname=plotdata1)
+    print('add data')
+    cerebro.adddata(stockdata)
+    cerebro.broker.setcash(100000.0)
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Run')
+    # Run over everything
+    cerebro.run()
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Plot the result
+    plotinfo = dict(subplot = True)
+    cerebro.plot(style='bar')
+
+
+runstrat()     
