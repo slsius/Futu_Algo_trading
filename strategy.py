528382b1730e36d8ab1d025dffaedf6d5afdc07c
slsius <ssiusinglung@gmail.com>
2020-06-08 08:22:36 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -11,8 +11,8 @@ class RVIin(bt.Indicator):
         
         
     def next(self):
-        NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6  
-        DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
+        NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3]) 
+        DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])
         if DEM ==0:
             self.lines.RVI[0] = 0
         else:
