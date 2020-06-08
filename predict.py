0bb4918d8f88e6759e5b5596c2716cc100d06e5a
slsius <ssiusinglung@gmail.com>
2020-06-08 08:38:11 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -20,12 +20,12 @@ def DayStr(Tday): #function to return date in specific format
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111) #make connection
 
 today = datetime.today()
-NumDay = 10 #set the number of day of data
+NumDay = 20 #set the number of day of data
 
 
 #data set 1
-#ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=150*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
-ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start='2009-12-11', end='2009-12-31', max_count=150*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
+ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start=DayStr(today - timedelta(days=NumDay)), end='', max_count=150*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
+#ret1, data1, page_req_key1 = quote_ctx.request_history_kline('HK.00700', start='2009-12-11', end='2009-12-31', max_count=150*NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
 if ret1 == RET_OK:
     print('ok')
     #print(data1)
