e94bb6ce0e6ae20399d2fbda86a12ac2af280ab9
slsius <ssiusinglung@gmail.com>
2020-06-12 15:21:00 +0800

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -9,35 +9,42 @@ import numpy as np
 import random
 import argparse
 
-#-----get data
-quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
 
-ret, data, page_req_key = quote_ctx.request_history_kline('HK.00700', start='2020-06-10', end='', max_count=1000, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
-if ret == RET_OK:
-    print('ok')
-else:
-    print('error:', data)
-
-print(data)    
+code = input("Stock code:")
 '''
-ret, realdata = quote_ctx.get_cur_kline('HK.59350', 50, ktype=SubType.K_3M, autype=AuType.QFQ)
-if ret == RET_OK:
-    print('ok')
-else:
-    print('error:', realdata)
-'''    
-quote_ctx.close() #close connection    
+while true:
+    print('loop')
+    time.sleep(15)
+'''
+#-----get data    
+def datacall(code)    
+    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    while len(str(code)) <= 4:
+        code = '0' + str(code)
+    ret, data, page_req_key = quote_ctx.request_history_kline('HK.' + code, start='2020-06-10', end='', max_count=1000, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+    if ret == RET_OK:
+        print('ok')
+    else:
+        print('error:', data)   
+    
+    quote_ctx.close() #close connection  
+def snap()
+    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    get_market_snapshot(self, code_list)
+    quote_ctx.close() #close connection 
+  
 #-----trade------
-pwd_unlock = '878900'
-trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)
-print(trd_ctx.unlock_trade(pwd_unlock))
-ret_code, info_data = trd_ctx.accinfo_query()
-print(info_data)
+def trade()
+    pwd_unlock = '878900'
+    trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)
+    print(trd_ctx.unlock_trade(pwd_unlock))
+    ret_code, info_data = trd_ctx.accinfo_query()
+    print(info_data)
 
-print(trd_ctx.position_list_query())
+    print(trd_ctx.position_list_query())
 
-print(trd_ctx.order_list_query())
+    print(trd_ctx.order_list_query())
 
 
-#print(trd_ctx.place_order(price=700.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY))
-trd_ctx.close()
+    #print(trd_ctx.place_order(price=700.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY))
+    trd_ctx.close()
