a7cb351ae93943bebf13941fccb7a918f7175608
slsius <ssiusinglung@gmail.com>
2020-06-12 14:49:45 +0800

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -12,18 +12,19 @@ import argparse
 #-----get data
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
 
-ret, data, page_req_key = quote_ctx.request_history_kline('HK.59350', start="", end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
+ret, data, page_req_key = quote_ctx.request_history_kline('HK.00700', start="", end='', max_count=110, fields=KL_FIELD.ALL, ktype=KLType.K_3M) 
 if ret == RET_OK:
     print('ok')
 else:
     print('error:', data)
 
+'''
 ret, realdata = quote_ctx.get_cur_kline('HK.59350', 50, ktype=SubType.K_3M, autype=AuType.QFQ)
 if ret == RET_OK:
     print('ok')
 else:
     print('error:', realdata)
-    
+'''    
 quote_ctx.close() #close connection    
 #-----trade------
 pwd_unlock = '878900'
