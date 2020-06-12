ba0d75ea482c4ebecbe314491f829418b0e2aaa3
slsius <ssiusinglung@gmail.com>
2020-06-12 14:22:42 +0800

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -12,9 +12,9 @@ import argparse
 
 pwd_unlock = '878900'
 trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)
+print(trd_ctx.unlock_trade(pwd_unlock))
 ret_code, info_data = trd_ctx.accinfo_query()
 print(info_data)
-#print(info_data.cash)
-#print(trd_ctx.unlock_trade(pwd_unlock))
+print(info_data.cash)
 #print(trd_ctx.place_order(price=700.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY))
 trd_ctx.close()
