f687cc3183cfe4dbd658e93b013900c00b5f6869
slsius <ssiusinglung@gmail.com>
2020-06-15 11:40:16 +0800

    Update empty.py

--- empty.py
+++ empty.py
@@ -2,3 +2,16 @@ from datetime import datetime
 
 now = datetime.now()
 print(now)
+
+today930 = now.replace(hour=9, minute=35, second=0, microsecond=0)
+today11 = now.replace(hour=11, minute=0, second=0, microsecond=0)
+today13 = now.replace(hour=13, minute=0, second=0, microsecond=0)
+today15 = now.replace(hour=15, minute=0, second=0, microsecond=0)
+
+print(today930)
+print(today11)
+print(today13)
+print(today15)
+
+if (now > today930 and < today11) or (now > today13 and now < today15)
+  print('trade')
