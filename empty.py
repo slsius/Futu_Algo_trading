26c9ced90e48ba746a1de6a2139e3db8b59f7b25
slsius <ssiusinglung@gmail.com>
2020-06-17 15:16:13 +0800

    Update empty.py

--- empty.py
+++ empty.py
@@ -1,4 +1,16 @@
 from datetime import datetime
+import pandas as pd
+import pandas_ta as ta
+import numpy as np
+import matplotlib.pylab as plt
+from futu import *
+import talib
+from talib import abstract
+import numpy as np
+import random
+import argparse
+import pdb
+import os
 
 now = datetime.now()
 print(now)
@@ -17,3 +29,33 @@ if (now > today930 and now < today11) or (now > today13 and now < today15):
   print('trade')
 else:
   print('no trade')
+
+count = 0
+size = 50
+code = 00981
+pwd_unlock = '878900'
+trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)
+print(trd_ctx.unlock_trade(pwd_unlock))
+    
+ret,orderinfo = trd_ctx.order_list_query(trd_env = TrdEnv.SIMULATE)
+datetime_object = datetime.strptime(orderinfo.iloc[-1].updated_time , '%Y-%m-%dd %H:%M:%S')
+diff = datetime_object - datetime.now()
+    
+if diff.second < 120:
+  return 0
+    #place order
+print(trd_ctx.place_order(OrderType = 'MARKET', qty=size*10, code='HK.' + code, trd_side=TrdSide.BUY,trd_env=TrdEnv.SIMULATE))
+    
+    #check successful trade
+while True:
+  time.sleep(5)
+  ret, query = trd_ctx.order_list_query(trd_env = TrdEnv.SIMULATE)
+  if query[-1].order_status == FILLED_ALL:
+    NumPos = NumPos + size
+    break
+  elif count < 12:
+    count +=1
+  else:
+    trd_ctx.cancel_all_order(trd_env = TrdEnv.SIMULATE)
+    break
+    trd_ctx.close()  
