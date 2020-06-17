252c5d302518bc5ae75b3ff290fac1bbdc943788
slsius <ssiusinglung@gmail.com>
2020-06-17 09:13:22 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 from futu import *
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
 
-ret, newdata = quote_ctx.get_cur_kline([HK.00700], 50, SubType.K_1M)
+ret, newdata = quote_ctx.get_cur_kline(['HK.00700'], 50, SubType.K_1M)
 if ret == RET_OK:
             print(newdata)
             #print(newdata['turnover_rate'][0])  
