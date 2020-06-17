ea64c263d6e149d39993745ab020bd0d35612e0b
slsius <ssiusinglung@gmail.com>
2020-06-17 09:12:19 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,4 +1,12 @@
 from futu import *
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
-print(quote_ctx.get_market_snapshot('HK.00700'))
+
+ret, newdata = quote_ctx.get_cur_kline([HK.00700], 50, SubType.K_1M)
+if ret == RET_OK:
+            print(newdata)
+            #print(newdata['turnover_rate'][0])  
+            #print(newdata['turnover_rate'].values.tolist()) 
+    else:
+            print('error:', newdata)
+        
 quote_ctx.close()
