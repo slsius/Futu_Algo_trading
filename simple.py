fc7cec8b897f8dbbb501e8d9adacaede9144de00
slsius <ssiusinglung@gmail.com>
2020-06-22 13:34:13 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -56,13 +56,8 @@ else:
     while ret != RET_OK:
         ret,position = trd_ctx.position_list_query(trd_env = TrdEnv.SIMULATE)
 print('~~~~~~~~position')        
-print(position.loc[position['code'] == 'HK.' + str(code)].qty)
-print(position.loc[position['code'] == 'HK.' + str(code)].index)
-print('~~~~~~~~~~~~~value')
-print(position.loc[position['code'] == 'HK.' + str(code)]['qty'].values)
-print(position.iloc[position.loc[position['code'] == 'HK.' + str(code)].index].qty)
-if position.loc[position['code'] == 'HK.' + str(code)].qty > 0:
-    NumPos = position.loc[position['code'] == 'HK.' + str(code)].qty
+if position.loc[position['code'] == 'HK.' + str(code)]['qty'].values > 0:
+    NumPos = position.loc[position['code'] == 'HK.' + str(code)].qty.values
 trd_ctx.close()
     
     
