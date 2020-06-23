8c517f1f50192177cc461b68555213c39844be28
slsius <ssiusinglung@gmail.com>
2020-06-23 15:51:19 +0800

    Update autotrade_sim.py

--- autotrade_sim.py
+++ autotrade_sim.py
@@ -240,7 +240,7 @@ def closeall(close):
         print(order.iloc[i].order_status)
         if order.iloc[i].order_status == 'SUBMITTED':
             print(order.iloc[i].order_id)
-            print(trd_ctx.modify_order(ModifyOrderOp.CANCEL,order.iloc[i].order_id	 ,price = close, qty = size))
+            print(trd_ctx.modify_order(ModifyOrderOp.CANCEL,str(order.iloc[i].order_id)	 ,price = close, qty = size))
             print(trd_ctx.modify_order(ModifyOrderOp.DELETE,order.iloc[i].order_id	 ,price = close, qty = size))
     trd_ctx.close()    
 #-----loop    
