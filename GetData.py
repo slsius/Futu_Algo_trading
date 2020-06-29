5c3cbb185e3e9b8718451803442a54736e0981c0
slsius <ssiusinglung@gmail.com>
2020-06-29 13:00:57 +0800

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -22,7 +22,7 @@ today = datetime.today()
 NumDay = 2000 #set the number of day of data
 
 #data set
-ret, data, page_req_key1 = quote_ctx.request_history_kline('HK.02013', start='2018-01-01', end='2019-12-31', max_count=NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_Day) 
+ret, data, page_req_key1 = quote_ctx.request_history_kline('HK.02013', start='2018-01-01', end='2019-12-31', max_count=NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
 
 if ret1 == RET_OK:
     print(data)
