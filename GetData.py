311b2fa0aed402bade3fd9f6aeb7430658c0aa89
slsius <ssiusinglung@gmail.com>
2020-06-29 13:02:10 +0800

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -24,7 +24,7 @@ NumDay = 2000 #set the number of day of data
 #data set
 ret, data, page_req_key1 = quote_ctx.request_history_kline('HK.02013', start='2018-01-01', end='2019-12-31', max_count=NumDay, fields=KL_FIELD.ALL, ktype=KLType.K_DAY) 
 
-if ret1 == RET_OK:
+if ret == RET_OK:
     print(data)
     print(data['code'][0])    # 取第一条的股票代码
     print(data['close'].values.tolist())   # 第一页收盘价转为list
