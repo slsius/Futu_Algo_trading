15cb6ca7decfbc62601459f65c52137cf5e51671
slsius <ssiusinglung@gmail.com>
2020-06-30 13:26:52 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -2,7 +2,7 @@ import backtrader as bt
 import backtrader.indicators as btind
 
 class RVIin(bt.Indicator):
-    lines = ('RVI','RVIR')
+    lines = ('RVI','RVIR','NUM','DEM;)
     plotinfo = dict(subplot=True)
     params = (('minperiod', 8),)
     maperiod = 0
@@ -11,14 +11,14 @@ class RVIin(bt.Indicator):
         
         
     def next(self):
-        NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6 
-        DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
+        self.lines.NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6 
+        self.lines.DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
         avNUM = 0
         avDEM = 0
         for i in range(0,self.maperiod-1,1):
-            avNUM = NUM[-i] + avNUM
+            avNUM = self.lines.NUM[-i] + avNUM
         for i in range(0,self.maperiod-1,1):
-            avDEM = DEM[-i] + avDEM
+            avDEM = self.lines.DEM[-i] + avDEM
         
         if avDEM ==0:
             self.lines.RVI[0] = 0
