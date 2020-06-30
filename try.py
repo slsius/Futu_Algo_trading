c1040b9b0e1864f82fbe7c0d36891997010cec88
slsius <ssiusinglung@gmail.com>
2020-06-30 13:29:40 +0800

    Update try.py

--- try.py
+++ try.py
@@ -148,7 +148,7 @@ cerebro.adddata(stockdata)
 
 hist = {'RSI period','RSI Hi','RSI Lo','Profit/Loss'}
 df = pd.DataFrame(columns = hist)
-for tstperiod in range (4,10,1):  # chang value here
+for tstperiod in range (5,7,1):  # chang value here
   for tsthi in range(69,70,1):
     for tstlo in range(11,12,1):
       for tstmova in range(5,7,1):
