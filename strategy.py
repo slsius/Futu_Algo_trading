cc7055862cd89ebd92bd72ed249f4be66464027a
slsius <ssiusinglung@gmail.com>
2020-06-30 13:07:03 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -5,7 +5,7 @@ class RVIin(bt.Indicator):
     lines = ('RVI','RVIR')
     plotinfo = dict(subplot=True)
     params = (('minperiod', 8),)
-    period = 0
+    maperiod = 0
     def __init__(self):
         self.addminperiod(self.params.minperiod)
         
@@ -13,8 +13,8 @@ class RVIin(bt.Indicator):
     def next(self):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6 
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
-        avNUM = bt.ind.SMA(NUM,period = period)
-        avDEM = bt.ind.SMA(DEM,period = period)
+        avNUM = bt.ind.SMA(NUM,period = self.maperiod)
+        avDEM = bt.ind.SMA(DEM,period = self.maperiod)
         
         
         if DEM ==0:
