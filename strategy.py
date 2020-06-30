1c215d0dd27982149269bf32e2f779125b16e424
slsius <ssiusinglung@gmail.com>
2020-06-30 13:11:20 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -13,14 +13,14 @@ class RVIin(bt.Indicator):
     def next(self):
         NUM = (self.data.close - self.data.open + 2*(self.data.close[-1] - self.data.open[-1]) + 2*(self.data.close[-2] - self.data.open[-2]) + self.data.close[-3] - self.data.open[-3])/6 
         DEM = (self.data.high - self.data.low + 2*(self.data.high[-1] - self.data.low[-1]) + 2*(self.data.high[-2] - self.data.low[-2]) + self.data.high[-3] - self.data.low[-3])/6
-        avNUM = bt.ind.SMA(NUM,period = self.maperiod)
-        avDEM = bt.ind.SMA(DEM,period = self.maperiod)
+        self.avNUM = bt.ind.SMA(NUM,period = self.maperiod)
+        self.avDEM = bt.ind.SMA(DEM,period = self.maperiod)
         
         
         if DEM ==0:
             self.lines.RVI[0] = 0
         else:
-            self.lines.RVI[0] = (avNUM[0])/(avDEM[0])
+            self.lines.RVI[0] = (self.avNUM)/(self.avDEM)
         try:
           self.lines.RVIR[0] = (self.lines.RVI + 2*self.lines.RVI[-1] + 2*self.lines.RVI[-2] + self.lines.RVI[-3])/6
         except (IndexError, KeyError):
