1ff3af05e1edf94a07e462afe25bf953cbfed391
slsius <ssiusinglung@gmail.com>
2020-07-10 12:29:09 +0800

    Update autotrade_sim.py

--- autotrade_sim.py
+++ autotrade_sim.py
@@ -310,6 +310,8 @@ while True:
         print('error:', data)
         while ret != RET_OK:
             ret, data = quote_ctx.get_cur_kline('HK.' + code, 30, SubType.K_3M, AuType.QFQ)
+    data['RVI'] = 0 #add column
+    ata['RVIR'] = 0 #add column
     signal(data)    #calculate the signal
     print('---------' + str(NumPos) + '--------')   #print number of holdings
     print('sell flag:' + str(sellflag))
