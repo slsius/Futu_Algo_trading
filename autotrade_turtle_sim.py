3c545ed45c45e7d21f9ca8bcc94a40d1baf98214
slsius <ssiusinglung@gmail.com>
2020-07-13 11:54:14 +0800

    Update autotrade_turtle_sim.py

--- autotrade_turtle_sim.py
+++ autotrade_turtle_sim.py
@@ -101,7 +101,7 @@ def signal(data):
             maDEM = maDEM + data.iloc[-i].Dem
         data.at[30-j,'RVI'] = (maNEM/RVIper)/(maDEM/RVIper)
     data.at[29,'RVIR'] = (data.iloc[-1].RVI + 2*data.iloc[-2].RVI + 2*data.iloc[-3].RVI + data.iloc[-4].RVI)/6   
-    print(data)
+    print(data.iloc[-3:)
     
     #when to in
     if NumPos == 0:
