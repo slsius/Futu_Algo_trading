d5e20ad20ea97b6654b5a2996b860f441d77f68d
slsius <ssiusinglung@gmail.com>
2020-07-15 13:39:16 +0800

    Update autotrade_turtle_sim.py

--- autotrade_turtle_sim.py
+++ autotrade_turtle_sim.py
@@ -221,7 +221,7 @@ def buy(close):
         else:
             while ret != RET_OK:
                 ret, query = trd_ctx.order_list_query(trd_env = TrdEnv.SIMULATE)
-        if query.loc[query.index['code' == 'HK.' + str(code)].min()].order_status == 'FILLED_ALL':
+        if query.iloc[query.index['code' == 'HK.' + str(code)].min()].order_status == 'FILLED_ALL':
             #orderinfo.iloc[orderinfo.index[orderinfo['code'] == 'HK.' + str(code)].max()].create_time
             NumPos = NumPos + size*hand #add lot size if success
             openprice = close
