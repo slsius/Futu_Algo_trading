1e832e10268f5d70585045cbda25992eb77ac043
slsius <ssiusinglung@gmail.com>
2020-07-21 09:54:07 +0800

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -33,9 +33,9 @@ print(order)
 print(order.index[order['code'] == 'HK.'+str(code)])
 print(order.index[order['trd_side'] == 'SELL']) 
 
-print(order.index[order['code'] == 'HK.'+str(code) & order['trd_side'] == 'SELL'])
+print(order.index[(order['code'] == 'HK.'+str(code)) & (order['trd_side'] == 'SELL')])
 
-#order.iloc[order.index[(order['code'] == 'HK.' + str(code)) & order['trd_side'] == 'SELL'].min()].order_status == 'FILLED_ALL':
+order.iloc[order.index[(order['code'] == 'HK.' + str(code)) & (order['trd_side'] == 'SELL')].min()].order_status == 'FILLED_ALL':
 
 quote_ctx.close()
 trd_ctx.close() #close connection
